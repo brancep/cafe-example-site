a0588afc984a78fa55ff90e0512af314ecc2ee17
Brance Pearson <brancepearson@gmail.com>
2017-12-12 23:21:09 -0800

    updated the Shops and Menu modal pages and included JS to show and hide.

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 
 
+    $('.modal-wrapper').hide();
+
 $(document).ready(function(){
-    $('#menu-nav').attr('a', href);
+
+    $('#menu-nav').on('click', function(){
+        $('#menu-modal').fadeIn('slow');
+        $('.nav-grid').hide();
+    });
+    
+    $('#shops-nav').on('click', function(){
+        $('#shops-modal').fadeIn( "slow" );
+        $('.nav-grid').hide();
+    });
+
+
+    // $('#crew-nav').on('click', function(){
+    //     $('#crew-modal').fadeIn(250);
+    //     $('.nav-grid').hide();
+    // });
+
+    // $('#events-nav').on('click', function(){
+    //     $('#events-modal').fadeIn(250);
+    //     $('.nav-grid').hide();
+    // });
+
+    $('.close-button').on('click', function(){
+        $('.modal-wrapper').fadeOut('slow');
+        $('.nav-grid').show();
+    });
 });
